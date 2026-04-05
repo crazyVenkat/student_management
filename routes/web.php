b059a9827ae26dc6dc91da502d559ceba6141723
Gobinath M <88788590+crazyVenkat@users.noreply.github.com>
2026-04-05 14:22:17 +0530

    Route changes

--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,30 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
-
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
     /* Students */
-    Route::get('/students', [StudentController::class, 'index'])->name('students.index');
-    Route::get('/students/list', [StudentController::class, 'list'])->name('students.list');
-    Route::post('/students/store', [StudentController::class, 'store'])->name('students.store');
-    Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
-    Route::post('/students/update/{id}', [StudentController::class, 'update']);
-    Route::delete('/students/delete/{id}', [StudentController::class, 'destroy']);
-    Route::post('/students/import', [StudentController::class, 'importStudents'])->name('students.import');
+    Route::prefix('students')->name('students.')->group(function () {
+        Route::get('/', [StudentController::class, 'index'])->name('index');
+        Route::get('/list', [StudentController::class, 'list'])->name('list');
+        Route::post('/store', [StudentController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('edit');
+        Route::post('/update/{id}', [StudentController::class, 'update'])->name('update');
+        Route::delete('/delete/{id}', [StudentController::class, 'destroy'])->name('destroy');
+        Route::post('/import', [StudentController::class, 'importStudents'])->name('import');
+    });
 
     /* Staffs */
-    Route::get('/staff', [StaffController::class, 'index'])->name('staff.index');
-    Route::get('/staff/list', [StaffController::class, 'list'])->name('staff.list');
-    Route::post('/staff/store', [StaffController::class, 'store']);
-    Route::get('/staff/{id}/edit', [StaffController::class, 'edit']);
-    Route::post('/staff/update/{id}', [StaffController::class, 'update']);
-    Route::delete('/staff/delete/{id}', [StaffController::class, 'destroy']);
+    Route::prefix('staff')->name('staff.')->group(function () {
+        Route::get('/', [StaffController::class, 'index'])->name('index');
+        Route::get('/list', [StaffController::class, 'list'])->name('list');
+        Route::post('/store', [StaffController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [StaffController::class, 'edit'])->name('edit');
+        Route::post('/update/{id}', [StaffController::class, 'update'])->name('update');
+        Route::delete('/delete/{id}', [StaffController::class, 'destroy'])->name('destroy');
+    });
 
     // Dependent dropdown
     Route::get('/programmes/{department_id}', [StudentController::class, 'getProgrammes']);
 });
 
-// Default redirect
-Route::get('/', function () {
-    return redirect('/login');
-});
